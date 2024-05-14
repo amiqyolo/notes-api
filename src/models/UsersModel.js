db5e7151ac498eb40f726c9b89819cd4b725e7b1
Amiq Dian <66551148+amiqyolo@users.noreply.github.com>
2024-05-14 18:56:25 +0700

    add UsersModel

---
+++ src/models/UsersModel.js
@@ -0,0 +1,40 @@
+import { Sequelize } from "sequelize";
+import db from "../config/Database.js";
+
+const Users = db.define('users', {
+    uuid: {
+        type: DataTypes.STRING,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false, // not allowed null value
+        validate: {
+            notEmpty: true
+        } // not allowed empty string
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+            len: [3, 100] // min 3 max 100
+        }
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+}, {
+    freezeTableName: true
+});
+
+export default Users;
