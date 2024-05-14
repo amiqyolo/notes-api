7bf7be7884965cf7eb856cb2c5c9815f79a3911c
Amiq Dian <66551148+amiqyolo@users.noreply.github.com>
2024-05-14 19:17:25 +0700

    generate table for notes, users, and session

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import FileUpload from "express-fileupload";
+import SequelizeStore from "connect-session-sequelize";
+import session from "express-session";
 import NotesRoute from "./routes/NotesRoute.js";
 import UsersRoute from "./routes/UsersRoute.js";
 import db from './config/Database.js';
+import Notes from "./models/NotesModel.js";
+dotenv.config(); // load .env file
 
 const app = express();
+
+const sessionStore = SequelizeStore(session.Store);
+
+const store = new sessionStore({
+    db: db
+});
 
 // (async() => {
 //     await db.sync();
 // })();
 
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: store,
+    cookie: {
+        secure: 'auto' // false to http, true for https
+    }
+}));
 app.use(cors());
 app.use(express.json());
 app.use(FileUpload());
@@ -20,7 +40,8 @@ app.use(UsersRoute)
 
 // Notes.sync();
 
-
+// generate session table
+// store.sync();
 
 const listener = app.listen(5000, () => {
     console.log(`Server running in PORT ${listener.address().port}`);
