68487f500350e0eff2f561c661f45fd23b9b9491
Amiq Dian <66551148+amiqyolo@users.noreply.github.com>
2024-05-14 17:39:18 +0700

    write getNotes controller

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -1,2 +1,10 @@
-import Notes from "../models/Notes";
+import Notes from "../models/NotesModel.js";
 
+export const getNotes = async(req, res) => {
+    try {
+        const response = Notes.findAll();
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
--- src/routes/NotesRoute.js
+++ src/routes/NotesRoute.js
@@ -1,5 +1,10 @@
 import express from "express";
+import { 
+    getNotes,
+} from "../controllers/NotesController.js";
 
 const route = express.Router();
+
+route.get('/notes', getNotes);
 
 export default route;
