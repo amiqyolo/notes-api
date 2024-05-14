dd2498dc8e0454ff6b4bef68dbb43036d00bd9eb
Amiq Dian <66551148+amiqyolo@users.noreply.github.com>
2024-05-14 21:05:19 +0700

    set get user login  controller and route

--- src/routes/AuthRoute.js
+++ src/routes/AuthRoute.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { login } from "../controllers/AuthController.js";
+import { 
+    getUser, 
+    login
+} from "../controllers/AuthController.js";
 
 const route = express.Router();
 
 route.post('/login', login);
+route.get('/user', getUser);
 
 export default route;
